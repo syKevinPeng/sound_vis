aedcf15e7b8ed7d21872d9342387d7c9796d3eb3
Siyuan Peng <peng2000@umd.edu>
2023-11-23 19:15:10 -0500

    upd

--- frontend/src/components/Progress.tsx
+++ frontend/src/components/Progress.tsx
@@ -1,16 +1,18 @@
+import { useLocation } from "preact-iso";
+
 const allProgress = {
-  Title: "Title",
-  Intro: "Intro",
-  Task1_0: "Task1_0",
-  Task1_1: "Task1_1",
-  Task1_2: "Task1_2",
-  Task1_3: "Task1_3",
-  Task2_0: "Task2_0",
-  Task2_1: "Task2_1",
-  Task2_2: "Task2_2",
+  Title: { text: "Title", link: "/" },
+  Intro: { text: "Intro", link: "/intro" },
+  Task1_0: { text: "Task1_0", link: "/task1/overview" },
+  Task1_1: { text: "Task1_1", link: "/task1/1" },
+  Task1_2: { text: "Task1_2", link: "/task1/2" },
+  Task1_3: { text: "Task1_3", link: "/task1/3" },
+  Task2_0: { text: "Task2_0", link: "/task2/overview" },
+  Task2_1: { text: "Task2_1", link: "/task2/1" },
+  Task2_2: { text: "Task2_2", link: "/task2/2" },
   // Task2_3: "Task2_3",
-  Task3_0: "Task3_0",
-  Task3_1: "Task3_1",
+  Task3_0: { text: "Task3_0", link: "/task3/overview" },
+  Task3_1: { text: "Task3_1", link: "/task3/1" },
   // Task3_2: "Task3_2",
   // Task3_3: "Task3_3",
 };
@@ -18,12 +20,30 @@ const allProgress = {
 export type Progress = keyof typeof allProgress;
 
 export default ({ progress }: { progress: Progress }) => {
+  const location = useLocation();
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      {Object.entries(allProgress).map(([k, v]) => (
-        <div style={k == progress ? { fontSize: "xx-large" } : {}} key={k}>
-          {v}
-        </div>
+    <div
+      style={{
+        flex: 1,
+        justifyContent: "space-evenly",
+        margin: "15vh 0",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      {Object.entries(allProgress).map(([k, { text, link }]) => (
+        <button
+          style={{
+            ...(k == progress ? { fontSize: "xx-large" } : {}),
+            cursor: "pointer",
+            backgroundColor: "transparent",
+            border: "none",
+          }}
+          key={k}
+          onClick={() => location.route(link)}
+        >
+          {text}
+        </button>
       ))}
     </div>
   );

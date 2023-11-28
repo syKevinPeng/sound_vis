ee2a39ffcd575b0848087ef0e7afc66817dd5bd7
SiddarGu <fenggu2000@gmail.com>
2023-11-28 17:13:34 -0500

    sidebar texts

--- frontend/src/components/Progress.tsx
+++ frontend/src/components/Progress.tsx
@@ -4,26 +4,26 @@ import { fpApi } from "@/index";
 let sectionNum = 1;
 
 export const allProgress = {
-  Title: { text: "Title", link: "/", sectionNum: sectionNum++ },
+  Title: { text: "Music Viz", link: "/", sectionNum: sectionNum++ },
   Intro: { text: "Introduction", link: "/intro", sectionNum: sectionNum++ },
   Task1_0: {
-    text: "Task1_0",
+    text: "Music vs. Noise Patterns",
     link: "/task1/overview",
     sectionNum: sectionNum++,
   },
   Task1_1: { text: "Music Vs. Noise Spectrogram", link: "/task1/1", sectionNum: sectionNum++ },
   Task1_2: { text: "Low Level Feature", link: "/task1/2", sectionNum: sectionNum++ },
-  Task1_3: { text: "Task1_3", link: "/task1/3", sectionNum: sectionNum++ },
+  Task1_3: { text: "Spiral in Time", link: "/task1/3", sectionNum: sectionNum++ },
   Task2_0: {
-    text: "Task2_0",
+    text: "Valence and Arousal",
     link: "/task2/overview",
     sectionNum: sectionNum++,
   },
   Task2_1: { text: "Spectral Contrast", link: "/task2/1", sectionNum: sectionNum++ },
-  Task2_2: { text: "Task2_2", link: "/task2/2", sectionNum: sectionNum++ },
+  Task2_2: { text: "Chromagrams", link: "/task2/2", sectionNum: sectionNum++ },
   // Task2_3: "Task2_3",
   Task3_0: {
-    text: "Task3_0",
+    text: "Physical Instruments",
     link: "/task3/overview",
     sectionNum: sectionNum++,
   },
